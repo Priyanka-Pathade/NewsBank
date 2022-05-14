2c44ed0c16031abda82d941afaf8018303e6bdae
Priyanka-Pathade <priyankapathade2870@gmail.com>
2022-05-14 22:37:04 +0530

    Pagination has been added

--- src/components/News.js
+++ src/components/News.js
@@ -8,21 +8,53 @@ export class News extends Component {
         console.log("Hello, I'm constructor from news!");
         this.state={
             articles: [],
-            loading:false
+            loading:false,
+            page:1
         }
-    }
+    }    
    async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=7beee3b7d25d4a10bba051a6caa099d6";
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=7beee3b7d25d4a10bba051a6caa099d6&page=1&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
-       console.log(parsedData);
-         this.setState({articles:parsedData.articles})
+        console.log(parsedData);
+        this.setState({
+            articles:parsedData.articles,
+            totalResults: parsedData.totalResults
+            })
      }
+     handlePrevClick =async()=>{
+        console.log("prev");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=7beee3b7d25d4a10bba051a6caa099d6&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            articles:parsedData.articles,
+            page : this.state.page-1
+        })
+     }
+     handleNextClick = async()=>{
+        //console.log("next")
+        if(this.state.page +1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
+
+        }
+        else{
+          let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=7beee3b7d25d4a10bba051a6caa099d6&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+          let data = await fetch(url);
+          let parsedData = await data.json();
+          console.log(parsedData);
+          this.setState({
+              articles:parsedData.articles,
+              page : this.state.page + 1
+          })
+      }
+    }
+  
   render() {
-      console.log("render");
+    //  console.log("render");
     return (
       <div className='container my-3'>
-          <h2>Newsbank - Top headlines</h2>
+        <h1 className="text-center">Newsbank - Top headlines</h1>
           
           <div className="row">
           {this.state.articles.map((element)=>{
@@ -32,7 +64,10 @@ export class News extends Component {
               </div>
             })}
           </div>
-         
+         <div className="container d-flex justify-content-between">
+             <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Prev</button>
+             <button  disabled={this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+         </div>
 
       </div>
     )
